81bdca98caa56cf86d69ba95d63130ec46864a97
jdoyon0000-png <jdoyon0000@gmail.com>
2026-02-28 22:37:02 -0500

    Enfin

--- pointfixe.py
+++ pointfixe.py
@@ -11,38 +11,25 @@ def Pointfixe(fonction_pointfixe, Q0, tolr, nmax):
             time.sleep(0.5)
             X[i] = Q_actuel
 
-        elif i == 1:
-            Q_suivant = fonction_pointfixe(Q_actuel)
-            
-            En_plus_1 = abs(Q_suivant - Q_actuel)
-
-            print(f'{i} Q_actuel = {Q_actuel:E}, Q_suivant = {Q_suivant:E}, err = {En_plus_1:E}')
-            time.sleep(0.5)
-
-            X[i] = Q_suivant
-
-            if En_plus_1 < tolr:
-                print('La méthode du point fixe a convergé!')
-                return X[:i+1]
-
-            Q_actuel = Q_suivant
-
         else:
+
             Q_suivant = fonction_pointfixe(Q_actuel)
+            En = abs(Q_suivant - Q_actuel)
             
 
-            En = abs(Q_actuel - X[i-2])           
-            En_plus_1 = abs(Q_suivant - Q_actuel)
+            Q_next = fonction_pointfixe(Q_suivant)
+            En_plus_1 = abs(Q_next - Q_suivant)
             
+
             ratio1 = En_plus_1 / En if En != 0 else 0
             ratio2 = En_plus_1 / (En**2) if En != 0 else 0
 
-            print(f'{i} Q_actuel = {Q_actuel:E}, Q_suivant = {Q_suivant:E}, err = {En_plus_1:E}, Ratio1 = {ratio1:E}, Ratio 2 = {ratio2:E}')
+            print(f'{i} Q_actuel = {Q_actuel:E}, err = {En:E}, Ratio1 = {ratio1:E}, Ratio 2 = {ratio2:E}')
             time.sleep(0.5)
 
             X[i] = Q_suivant
 
-            if En_plus_1 < tolr:
+            if En < tolr:
                 print('La méthode du point fixe a convergé!')
                 return X[:i+1]
 
