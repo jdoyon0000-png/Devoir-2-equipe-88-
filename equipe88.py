9f362018d767dea862ea785a53a5b9db398636d6
Alex GB <alexgb308@gmail.com>
2026-02-28 19:03:27 -0500

    ok

--- equipe88.py
+++ equipe88.py
@@ -10,6 +10,5 @@ from pointfixe import Pointfixe
 def F(Q):
     return math.e**Q + Q/2 -5
 
-print(Bissection(F,1,2,0.000005,18))
-#l'erreur (err) en n = 18 est err = 3.814697E-06 = 0,3814697E-5
+print(Bissection(F,1,2,0.00005,15))
 
