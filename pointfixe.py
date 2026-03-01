b22db27ec03aaa4f6cf95cce9fa3742c848a47cb
jdoyon0000-png <jdoyon0000@gmail.com>
2026-02-28 23:14:15 -0500

    My guy

--- pointfixe.py
+++ pointfixe.py
@@ -1,39 +1,56 @@
 import time
 
+
 def Pointfixe(fonction_pointfixe, Q0, tolr, nmax):
+
     X = [0] * (nmax + 1)
-    Q_actuel = Q0
-
+    
+    Q_prev = Q0
+    Q_curr = Q0
+    
     for i in range(nmax + 1):
-
         if i == 0:
-            print(f'{i} Q0 = {Q_actuel:E}')
-            time.sleep(0.5)
-            X[i] = Q_actuel
+            X[i] = Q0
 
+            print(f'{i} Qn = {Q0:E}')
+            time.sleep(0.5)
+            
+
+            Q_prev = Q0
+            Q_curr = fonction_pointfixe(Q0)
+            
         else:
-
-            Q_suivant = fonction_pointfixe(Q_actuel)
-            En = abs(Q_suivant - Q_actuel)
+            X[i] = Q_curr
+            e_n = abs(Q_curr - Q_prev)
             
 
-            Q_next = fonction_pointfixe(Q_suivant)
-            En_plus_1 = abs(Q_next - Q_suivant)
+            err_rel = e_n / abs(Q_curr) if Q_curr != 0 else e_n
             
 
-            ratio1 = En_plus_1 / En if En != 0 else 0
-            ratio2 = En_plus_1 / (En**2) if En != 0 else 0
-
-            print(f'{i} Q_actuel = {Q_actuel:E}, err = {En:E}, Ratio1 = {ratio1:E}, Ratio 2 = {ratio2:E}')
-            time.sleep(0.5)
-
-            X[i] = Q_suivant
-
-            if En < tolr:
-                print('La méthode du point fixe a convergé!')
+            if err_rel < tolr or i == nmax:
+                print(f'{i} Qn = {Q_curr:E}, err = {e_n:E}')
+                time.sleep(0.5)
+                
+                if err_rel < tolr:
+                    print('La méthode du point fixe a convergé!')
+                else:
+                    print('La méthode du point fixe n\'a pas convergé...')
                 return X[:i+1]
+                
+            else:
 
-            Q_actuel = Q_suivant
+                Q_next = fonction_pointfixe(Q_curr)
+                e_n_plus_1 = abs(Q_next - Q_curr)
+                
+                ratio1 = e_n_plus_1 / e_n
+                ratio2 = e_n_plus_1 / (e_n**2)
+                ratio3 = e_n_plus_1 / (e_n**3)
+                
 
-    print('La méthode du point fixe n\'a pas convergé...')
+                print(f'{i} Qn = {Q_curr:E}, err = {e_n:E}, Ratio1 = {ratio1:E}, Ratio2 = {ratio2:E}, Ratio3 = {ratio3:E}')
+                time.sleep(0.5)
+
+                Q_prev = Q_curr
+                Q_curr = Q_next
+
     return X
