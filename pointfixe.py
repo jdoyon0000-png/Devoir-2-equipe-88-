70a0d5e6cc7785a1b08bebec552ed4ffc6542015
Alex GB <alexgb308@gmail.com>
2026-03-02 19:04:01 -0500

    e

--- pointfixe.py
+++ pointfixe.py
@@ -5,8 +5,8 @@ def Pointfixe(fonction_pointfixe, Q0, tolr, nmax):
 
     X = [0] * (nmax + 1)
     
-    Q_prev = Q0
-    Q_curr = Q0
+    Q_avant = Q0
+    Qn = Q0
     
     for i in range(nmax + 1):
         if i == 0:
@@ -16,19 +16,19 @@ def Pointfixe(fonction_pointfixe, Q0, tolr, nmax):
             time.sleep(0.5)
             
 
-            Q_prev = Q0
-            Q_curr = fonction_pointfixe(Q0)
+            Q_avant = Q0
+            Qn = fonction_pointfixe(Q0)
             
         else:
-            X[i] = Q_curr
-            e_n = abs(Q_curr - Q_prev)
+            X[i] = Qn
+            e_n = abs(Qn - Q_avant)
             
 
-            err_rel = e_n / abs(Q_curr) if Q_curr != 0 else e_n
+            err_rel = e_n / abs(Qn) if Qn != 0 else e_n
             
 
             if err_rel < tolr or i == nmax:
-                print(f'{i} Qn = {Q_curr:E}, err = {e_n:E}')
+                print(f'{i} Qn = {Qn:E}, err = {e_n:E}')
                 time.sleep(0.5)
                 
                 if err_rel < tolr:
@@ -39,18 +39,18 @@ def Pointfixe(fonction_pointfixe, Q0, tolr, nmax):
                 
             else:
 
-                Q_next = fonction_pointfixe(Q_curr)
-                e_n_plus_1 = abs(Q_next - Q_curr)
+                Q_next = fonction_pointfixe(Qn)
+                e_n_plus1 = abs(Q_next - Qn)
                 
-                ratio1 = e_n_plus_1 / e_n
-                ratio2 = e_n_plus_1 / (e_n**2)
-                ratio3 = e_n_plus_1 / (e_n**3)
+                ratio1 = e_n_plus1 / e_n
+                ratio2 = e_n_plus1 / (e_n**2)
+                ratio3 = e_n_plus1 / (e_n**3)
                 
 
-                print(f'{i} Qn = {Q_curr:E}, err = {e_n:E}, Ratio1 = {ratio1:E}, Ratio2 = {ratio2:E}, Ratio3 = {ratio3:E}')
+                print(f'{i} Qn = {Qn:E}, err = {e_n:E}, Ratio1 = {ratio1:E}, Ratio2 = {ratio2:E}, Ratio3 = {ratio3:E}')
                 time.sleep(0.5)
 
-                Q_prev = Q_curr
-                Q_curr = Q_next
+                Q_avant = Qn
+                Qn = Q_next
 
     return X
